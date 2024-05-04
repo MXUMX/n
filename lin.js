5ac050cf0ce9a920975556f70df15480feed2576
MXUMX <119454462+MXUMX@users.noreply.github.com>
2024-05-04 19:00:11 -0400

    Update lin.js

--- lin.js
+++ lin.js
@@ -1,4 +1,4 @@
-fetch('test.txt')
+fetch('/n/lins.txt')
     .then(response => response.text())
     .then(data => console.log(data))
     .catch(error => console.error('Error fetching file:', error));
