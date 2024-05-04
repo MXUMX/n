332b983551bc1b2529f7a013341007379ef6815e
MXUMX <119454462+MXUMX@users.noreply.github.com>
2024-05-04 16:35:34 -0400

    Update func.js

--- func.js
+++ func.js
@@ -1,11 +1,19 @@
 function func () {
 
-  const w = window.open() // access the "about:blank" window you've opened
-  w.document.body.innerHTML = "<iframe id="myiframe" src='https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/version@e14f9d7fa2d076a4845a22c5b0e566f7cfd0ac4b/Getaway-Shootout.xml&container=ig' width='100%' height='100%' style="border:none;" frameBorder="0"></iframe> <script>var iframe = document.getElementById("myiframe");iframe.width = iframe.contentWindow.document.body.scrollWidth;iframe.height = iframe.contentWindow.document.body.scrollHeight;</script>"
+  const w = window.open(); // access the "about:blank" window you've opened
+  w.document.body.innerHTML = `
+  <iframe id="myiframe" src='https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/version@e14f9d7fa2d076a4845a22c5b0e566f7cfd0ac4b/Getaway-Shootout.xml&container=ig' width='100%' height='100%' style="border:none;" frameBorder="0">
+  </iframe> 
+  <script>
+    var iframe = document.getElementById("myiframe");
+    iframe.width = iframe.contentWindow.document.body.scrollWidth;
+    iframe.height = iframe.contentWindow.document.body.scrollHeight;
+  </script>
+  `;
   // or access other parts to add more JS or CSS
-  const style = w.document.createElement("link")
-  link.href = "/path/to.css"
-  link.rel = "stylesheet"
-  w.document.head.appendChild(style)
+  const style = w.document.createElement("link");
+  link.href = "/path/to.css";
+  link.rel = "stylesheet";
+  w.document.head.appendChild(style);
 
-}
+};
