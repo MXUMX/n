74357bf51dddac42dfa7de04f91add17cc5289b9
MXUMX <119454462+MXUMX@users.noreply.github.com>
2024-05-03 20:58:30 -0400

    Update name.js

--- name.js
+++ name.js
@@ -1,7 +1,11 @@
-const w = window.open() // access the "about:blank" window you've opened
-w.document.body.innerHTML = "<iframe src='https://www.discord.com/login/' width='100%' height='400' frameborder='1'></iframe>"
-// or access other parts to add more JS or CSS
-const style = w.document.createElement("link")
-link.href = "/path/to.css"
-link.rel = "stylesheet"
-w.document.head.appendChild(style)
+function myfunction () {
+
+  const w = window.open() // access the "about:blank" window you've opened
+  w.document.body.innerHTML = "<iframe src='https://www.discord.com/login/' width='100%' height='400' frameborder='1'></iframe>"
+  // or access other parts to add more JS or CSS
+  const style = w.document.createElement("link")
+  link.href = "/path/to.css"
+  link.rel = "stylesheet"
+  w.document.head.appendChild(style)
+
+}
