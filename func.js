caf0e59b3570fd8b3a7cfce747736455e3a094f1
MXUMX <119454462+MXUMX@users.noreply.github.com>
2024-05-04 12:28:25 -0400

    Update func.js

--- func.js
+++ func.js
@@ -1,7 +1,7 @@
 function func () {
 
   const w = window.open() // access the "about:blank" window you've opened
-  w.document.body.innerHTML = "<iframe src='https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/version@e14f9d7fa2d076a4845a22c5b0e566f7cfd0ac4b/Getaway-Shootout.xml&container=ig' width='100%' height='100%' frameborder='1'></iframe>"
+  w.document.body.innerHTML = "<iframe src='https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/version@e14f9d7fa2d076a4845a22c5b0e566f7cfd0ac4b/Getaway-Shootout.xml&container=ig' width='100%' height='100%' frameBorder="0"></iframe>"
   // or access other parts to add more JS or CSS
   const style = w.document.createElement("link")
   link.href = "/path/to.css"
